41c8cb27e7452ce28de3df62af16682b32fa66b9
Reuter, Niels <n.reuter@fz-juelich.de>
2021-02-01 15:31:50 +0100

    Add pole support

--- raritan/structures.py
+++ raritan/structures.py
@@ -18,40 +18,77 @@ logger = logging.getLogger('raritan_exporter')
 
 
 class PDU(object):
-    """Raritan power distribution unit (PDU) data object.
+    model_requests = [
+        ['/model/pdu/0', 'getInlets', 'inlet'],
+        ['/model/pdu/0', 'getOutlets', 'outlet'],
+        ['/model/peripheraldevicemanager', 'getDeviceSlots', 'device']
+    ]
 
-    Sets up a connection to the bulk json-rpc interface for Raritan PDUs and
-    requests all connectors (inlets, outlets, and device slots) and their
-    respective sensors. Sensors can subsequently be read to obtain up-to-date
-    values.
-
-    Parameters
-    ----------
-    name : str
-        name of the PDU. Only used for logging purposes.
-    location : str
-        Internet address of the PDU, including the protocol (e.g., http:// or
-        https://)
-    auth : tuple, optional
-        the username and password combination for logging into the PDU from 
-        its internet address
-    insecure : bool, optional
-        whether to allow an insecure connection to the raritan PDU
-    """
     def __init__(self, location: str, name: Optional[str] = None,
                  auth: Optional[tuple] = (), insecure: Optional[bool] = True):
+        """Raritan power distribution unit (PDU) data object.
+
+        Sets up a connection to the bulk json-rpc interface for Raritan PDUs
+        and requests all connectors (inlets, outlets, and device slots) and
+        their respective sensors. Sensors can subsequently be read to obtain
+        up-to-date values.
+
+        Parameters
+        ----------
+        name : str
+            name of the PDU. Only used for logging purposes.
+        location : str
+            Internet address of the PDU, including the protocol (e.g., http://
+            or https://)
+        auth : tuple, optional
+            the username and password combination for logging into the PDU from
+            its internet address
+        insecure : bool, optional
+            whether to allow an insecure connection to the raritan PDU
+        """
         self.location = urlparse(location).netloc
         self.name = name if name is not None else self.location
         self.uri_pdu = '/model/pdu/0'
         self.uri_device = '/model/peripheraldevicemanager'
-        self.client = self._http_client(
-            endpoint=urljoin(location, '/bulk'),
-            auth=auth,
-            verify=not insecure
-        )
         self.connectors = []
+        self.poles = []
         self.sensors = []
-        self.phases = []
+
+        # HTTP Client
+        logger.info(f'({self.name}) polling at {location}')
+        client = HTTPClient(urljoin(location, '/bulk'))
+        client.session.auth = auth
+        client.session.verify = not insecure
+        client.session.headers.update({"Content-Type": "application/json-rpc"})
+        retry = Retry(connect=1)  # max. 1 retry to prevent pool overflows
+        adapter = HTTPAdapter(max_retries=retry)
+        client.session.mount('http://', adapter)
+        client.session.mount('https://', adapter)
+        self.client = client
+
+    def send(self, request: Request, **kwargs):
+        return self.client.send(request, timeout=10, **kwargs)
+
+    def bulk(self, req: list, **kwargs):
+        return self.client.send(Request('performBulk', req), timeout=10,
+                                **kwargs)
+
+    def get_sources(self):  # rename to crawl()
+        """request all sources (connectors and their sensors) from the PDU"""
+        self._get_connectors()
+        self._get_poles()
+        self._get_sensors()
+
+        n_inlets = len([c for c in self.connectors if c.type == 'inlet'])
+        n_outlets = len([c for c in self.connectors if c.type == 'outlet'])
+        n_devices = len([c for c in self.connectors if c.type == 'device'])
+        n_poles = len(self.poles)
+        n_sensors = len(self.sensors)
+        logger.info(
+            '(%s) %s inlet(s), %s outlet(s), %s pole(s) and %s device(s) with '
+            'a total of %s sensor(s) found'
+            % (self.name, n_inlets, n_outlets, n_poles, n_devices, n_sensors)
+        )
 
     def _http_client(self, endpoint: str,
                      auth: Optional[tuple] = (),
@@ -71,154 +108,117 @@ class PDU(object):
 
         return client
 
-    def send(self, request: Request, **kwargs):
-        return self.client.send(request, timeout=10, **kwargs)
-
-    def get_sources(self):
-        """request all sources (connectors and their sensors) from the PDU"""
+    def _get_connectors(self):
+        """Find all connectors and retrieve all associated meta-data"""
         # Get connector RIDs
-        n_inlets, n_outlets, n_devices, n_sensors = (0, 0, 0, 0)
-        query = {
-            'requests': [{
-                'rid': self.uri_pdu,
-                'json': Request('getInlets', request_id='inlet')
-            }, {
-                'rid': self.uri_pdu,
-                'json': Request('getOutlets', request_id='outlet')
-            }, {
-                'rid': self.uri_device,
-                'json': Request('getDeviceSlots', request_id='device')
-            }]
-        }
-        response = self.send(Request('performBulk', **query))
-        responses = response.data.result['responses']
+        response = self.bulk([
+            {'rid': m[0], 'json': Request(m[1], request_id=m[2])}
+            for m in self.model_requests
+        ])
         self.connectors = [
             Connector(rid=ret['rid'], type_=resp['json']['id'], parent=self)
-            for resp in responses
+            for resp in response.data.result['responses']
             for ret in resp['json']['result']['_ret_']
         ]
 
         # Get connector metadata
-        query = {'requests': [
+        response = self.bulk([
             {'rid': c.rid, 'json': Request('getMetaData', request_id=i)}
             for i, c in enumerate(self.connectors)
             if c.type != 'device'  # devices have no metadata
-        ]}
-        response = self.send(Request('performBulk', **query))
-        responses = response.data.result['responses']
-
-        for resp in responses:
-            connector = self.connectors[resp['json']['id']]
-            connector.update('metadata', **resp['json']['result']['_ret_'])
-
-        # Get connector settings
-        query = {'requests': [
-            {'rid': c.rid, 'json': Request('getSettings', request_id=i)}
-            for i, c in enumerate(self.connectors)
-        ]}
-        response = self.send(Request('performBulk', **query))
-        responses = response.data.result['responses']
-
-        for resp in responses:
-            connector = self.connectors[resp['json']['id']]
-            connector.update('settings', **resp['json']['result']['_ret_'])
-
-        # Get sensors per connector
-        query = {'requests': []}
-        for i, c in enumerate(self.connectors):
-            if c.type == 'inlet' or c.type == 'outlet':
-                method = 'getSensors'
-            elif c.type == 'device':
-                method = 'getDevice'
-            else:
-                raise ValueError('Unrecognized type %s' % c.type)
-
-            query['requests'].append(
-                {'rid': c.rid, 'json': Request(method, request_id=i)}
+        ])
+        for resp in response.data.result['responses']:
+            self.connectors[resp['json']['id']].update(
+                'metadata', **resp['json']['result']['_ret_']
             )
 
-        response = self.send(Request('performBulk', **query))
-        responses = response.data.result['responses']
+        # Get connector settings
+        response = self.bulk([
+            {'rid': c.rid, 'json': Request('getSettings', request_id=i)}
+            for i, c in enumerate(self.connectors)
+        ])
+        for resp in response.data.result['responses']:
+            self.connectors[resp['json']['id']].update(
+                'settings', **resp['json']['result']['_ret_']
+            )
 
-        for resp in responses:
+    def _get_poles(self):
+        """Get inlet poles and their associated sensors. This is done
+        outside of self._get_sensors due to the different structure of the
+        output"""
+        # Get pole RIDs
+        inlets = [c for c in self.connectors if c.type == 'inlet']
+        response = self.bulk([
+            {'rid': c.rid, 'json': Request('getPoles', request_id=i)}
+            for i, c in enumerate(inlets)
+        ])
+
+        for resp in response.data.result['responses']:
+            for pole in resp['json']['result']['_ret_']:
+                self.poles.append(
+                    Pole(
+                        label=pole['label'],
+                        line=pole['line'],
+                        node_id=pole['nodeId'],
+                        parent=inlets[resp['json']['id']],
+                    )
+                )
+
+                # Get associated sensors
+                for metric, data in pole.items():
+                    non_metrics = ['label', 'line', 'nodeId']
+                    if metric not in non_metrics and data is not None:
+                        self.sensors.append(
+                            Sensor(
+                                rid=data['rid'],
+                                interface=data['type'],
+                                parent=self.poles[-1],
+                                name=metric
+                            )
+                        )
+
+    def _get_sensors(self):
+        """Obtain sensor URI's and meta-data for each connector"""
+        # Get sensor RIDs from connectors
+        response = self.bulk([
+            {'rid': c.rid, 'json': Request(c.get_sensors, request_id=i)}
+            for i, c in enumerate(self.connectors)
+        ])
+        for resp in response.data.result['responses']:
             connector = self.connectors[resp['json']['id']]
             ret = resp['json']['result']['_ret_']
 
-            if connector.type == 'device':  # one sensor only
-                if ret is None:  # unused device slots return None
-                    continue
-
-                rid = ret['value']['device']['rid']
-                type_ = ret['value']['device']['type']
-                self.sensors.append(Sensor(rid, type_, connector))
-                n_devices += 1
-                n_sensors += 1
-
-            elif connector.type in ['inlet', 'outlet']:  # multiple sensors
-                if connector.type == 'inlet':
-                    n_inlets += 1
-                elif connector.type == 'outlet':
-                    n_outlets += 1
-
-                for metric, data in ret.items():
-                    if data is None:  # unused sensors return None
-                        continue
-
-                    rid = data['rid']
-                    type_ = data['type']
-                    self.sensors.append(Sensor(rid, type_, connector, metric))
-                    n_sensors += 1
+            if connector.type == 'device' and ret is not None:
+                # connectors w/ one sensor; returns no data if unused
+                self.sensors.append(
+                    Sensor(
+                        rid=ret['value']['device']['rid'],
+                        interface=ret['value']['device']['type'],
+                        parent=connector
+                    )
+                )
+            elif connector.type in ['inlet', 'outlet']:
+                # connectors w/ multiple sensors; returns no data if unused
+                for metric, data in ret.items:
+                    if data is not None:
+                        self.sensors.append(
+                            Sensor(
+                                rid=data['rid'],
+                                interface=data['type'],
+                                parent=connector,
+                                name=metric
+                            )
+                        )
 
         # Get sensor metadata
-        query = {'requests': [
+        response = self.bulk([
             {'rid': s.rid, 'json': Request('getMetaData', request_id=i)}
             for i, s in enumerate(self.sensors)
             if s.interface not in SENSORS_STATE  # these have no metadata
-        ]}
-        response = self.send(Request('performBulk', **query))
-        responses = response.data.result['responses']
-
-        for resp in responses:
-            sensor = self.sensors[resp['json']['id']]
-            sensor.update(**resp['json']['result']['_ret_'])
-
-        # Get phase sensors (they are not listed in the connectors)
-        self.get_phases()
-
-        logger.info(
-            '(%s) %s inlet(s), %s outlet(s), and %s device(s) with '
-            'a total of %s sensor(s) found'
-            % (self.name, n_inlets, n_outlets, n_devices, n_sensors)
-        )
-
-    def get_phases(self):
-        """Custom function to hard-code phase sensors"""
-        phases = {
-            'L1': '/tfwopaque/sensors.NumericSensor:4.0.3/I0P0Current',
-            'L2': '/tfwopaque/sensors.NumericSensor:4.0.3/I0P1Current',
-            'L3': '/tfwopaque/sensors.NumericSensor:4.0.3/I0P2Current'
-        }
-
-        for k, v in phases.items():
-            connector = Connector(
-                rid=self.uri_pdu,
-                type_='phase',
-                parent=self,
-            )
-            connector.update(
-                method='metadata', **{'label': k, 'type': 'inlet'}
-            )
-            self.connectors.append(connector)
-            sensor = Sensor(
-                rid=v,
-                interface='sensors.NumericSensor:4.0.3',
-                parent=connector,
-                name='phase',
-                unit='ampere',
-                metric='current'
-            )
-            sensor.update(**{'type': {'type': 2, 'unit': 2}})
-            self.sensors.append(sensor)
+        ])
+        for r in response.data.result['responses']:
+            self.sensors[r['json']['id']].update(
+                **r['json']['result']['_ret_'])
 
     def clear_sensors(self):
         for sensor in self.sensors:
@@ -276,22 +276,27 @@ class PDU(object):
 
 
 class Connector(object):
-    """Stores connector (inlet, outlet, or device slot) data.
+    method_by_type = {
+        'inlet': 'getSensors', 'outlet': 'getSensors', 'device': 'getDevice'
+    }
 
-    Parameters
-    ----------
-    rid : str
-        RID of the connector, returned by a json-rpc request for the 
-        getInlets, getOutlets, or getDeviceSlots methods to their respective
-        URIs
-    type_ : str
-        The connector type (either inlet, outlet, or device)
-    parent : PDU, optional
-        The PDU object this connector belongs to
-    """
     def __init__(self, rid: str, type_: str, parent: Optional[PDU] = None):
+        """Stores connector (inlet, outlet, or device slot) data.
+
+        Parameters
+        ----------
+        rid : str
+            RID of the connector, returned by a json-rpc request for the
+            getInlets, getOutlets, or getDeviceSlots methods to their
+            respective URIs
+        type_ : str
+            The connector type (either inlet, outlet, or device)
+        parent : PDU, optional
+            The PDU object this connector belongs to
+        """
         self.rid = rid
         self.type = type_
+        self.get_sensors = self.method_by_type[type_]
         self.parent = parent
         self.socket = None  # plug or receptacle
         self.label = rid.rsplit('/', 1)[-1]
@@ -311,6 +316,27 @@ class Connector(object):
         elif method == 'settings':
             if kwargs.get('name', None):
                 self.custom_label = kwargs['name']
+
+
+class Pole(object):
+    def __init__(self, label: str, line: int, node_id: int,
+                 parent: Optional[PDU] = None):
+        """Stores pole data.
+
+        Unlike other object types, poles don't have an RID. Instead, they
+        belong to the Inlet Connector.
+
+        Parameters
+        ----------
+        type_ : str
+            The pole sensor type
+        parent : Connector, optional
+            The Connector object this pole belongs to
+        """
+        self.type = 'pole'
+        self.label = label if label else f'L{line+1}'
+        self.line = line
+        self.node_id = node_id
 
 
 class Sensor(object):
